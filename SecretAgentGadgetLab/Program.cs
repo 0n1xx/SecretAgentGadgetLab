bf3ddcc39403e8fff197249e0e96aca23ae756ae
Vladislav Sakharov <200625361@student.georgianc.on.ca>
2026-04-04 19:55:44 -0400

    Fixes for the deployment

--- SecretAgentGadgetLab/Program.cs
+++ SecretAgentGadgetLab/Program.cs
@@ -58,11 +58,5 @@ app.MapControllerRoute(
 
 app.MapRazorPages()
    .WithStaticAssets();
-
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    db.Database.Migrate();
-}
 app.UseDeveloperExceptionPage();
 app.Run();
