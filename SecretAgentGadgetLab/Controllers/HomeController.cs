d35ac6e6506b9498e0dd46e85c2a56454bb12cd7
Vladislav Sakharov <200625361@student.georgianc.on.ca>
2026-04-04 21:15:21 -0400

    Adds Privacy page

--- SecretAgentGadgetLab/Controllers/HomeController.cs
+++ SecretAgentGadgetLab/Controllers/HomeController.cs
@@ -16,5 +16,10 @@ namespace SecretAgentGadgetLab.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
     }
 }
